544d0272626a64f0ce0a18e8f1f245271245c8ec
Evgeniya Chernyavskaya <chev640@gmail.com>
2024-01-25 12:50:28 +0200

    fix: fix errer writing output to file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -11,13 +11,14 @@ const writeable = createWriteStream(filePath);
 
 process.stdout.write('Hello! To write some information to a file, enter it here:' + '\n');
 
-process.stdin.pipe(writeable);
+
 process.stdin.on('data', chunk => {
   if (chunk.toString().trim().toLowerCase() == 'exit') {
     process.stdout.write('\n' + 'Thank you, your information is recorded in a file. Goodbay!' + '\n');
     process.exit(0);
   }
 });
+process.stdin.pipe(writeable);
 process.on('SIGINT', () => {
   process.stdout.write('\n' + 'Thank you, your information is recorded in a file. Goodbay!' + '\n');
   process.exit(0);
